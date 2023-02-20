bebb8267512795688dbf51c563ee541ab3a08d9b
saaicca <saaicca@gmail.com>
2023-02-20 13:56:30 +0800

    setup files

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// Declare a variable to store a DOM element
+const button = document.querySelector('#qa-portfolio-button');
+
+// Add an event listener to the button
+button.addEventListener('click', () => {
+    // When the button is clicked, execute this code
+    alert('Button clicked!');
+});
